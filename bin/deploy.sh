31e59733ba81e745e6eced6b8b8e36a546be9392
tyage <namatyage@gmail.com>
2015-12-02 02:46:21 +0900

    travis setting (thanks http://qiita.com/hakatashi/items/4f0034200dfd876c4fda)

---
+++ bin/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Exit on error
+set -ev
+
+if [ "$TRAVIS_SECURE_ENV_VARS" != "true" ] || [ "$TRAVIS_NODE_VERSION" != "node" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
+	exit 0
+fi
+
+# Convert clone to the full clone
+git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+git fetch --unshallow
+
+# Set identification
+git config user.name "Travis CI"
+git config user.email "namatyage@gmail.com"
+
+# Discard any changes in build
+git checkout -- .
+
+# Checkout and merge
+git checkout gh-pages
+git merge -X theirs "$TRAVIS_COMMIT" --no-edit
+
+# Push it all
+git push "https://${GH_TOKEN}@github.com/tyage/pripara-simulator.git" gh-pages:gh-pages --follow-tags > /dev/null 2>&1
